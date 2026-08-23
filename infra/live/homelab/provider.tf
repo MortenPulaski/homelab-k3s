1a6d4bc3fb8a5cfe414e51d70bba4e6e933f66a1
Marcin <marcin@marpal-it.de>
2026-08-23 16:38:26 +0200

    feat: Proxmox-Provider konfiguriert, Lock-File nicht mehr ignoriert

---
+++ infra/live/homelab/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://pve2.marpal-it.de"
+  # api_token wird NICHT hier eingetragen, sondern über die
+  # Umgebungsvariable PROXMOX_VE_API_TOKEN aus secrets.sops.yaml geladen
+  # (von mise automatisch bereitgestellt)
+}

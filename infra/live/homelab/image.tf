456d396476caec5368336bc3a179f951920b63af
Marcin <marcin@marpal-it.de>
2026-08-26 20:53:20 +0200

    Update Debian cloud image checksum (upstream point release changed)

--- infra/live/homelab/image.tf
+++ infra/live/homelab/image.tf
@@ -9,7 +9,7 @@ resource "proxmox_download_file" "debian13" {
 
   # Integrität (empfohlen, passt zu deinem Supply-Chain-Fokus):
   # SHA512 aus https://cloud.debian.org/images/cloud/trixie/latest/SHA512SUMS holen
-  checksum           = "77429b411b39b43f914dc9d14bf34aa315489a1a12b5429f72e5b483bdda23c65698d33443c85d3f3ad7c3a0828ae60845406d6b99646342554d17abae29c2a3"
+  checksum           = "184761b0dad0f9ace02f9298050ca96ce3caa39a461a47706d47ff9698b59933918b91b40177fbd4d392f6446af8b4d18ecb94caca988169b19641606bf34003"
   checksum_algorithm = "sha512"
 
   overwrite = false
